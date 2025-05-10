721691fd3cbccc84a0ea7f672033c041f1a23b2a
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2025-05-10 19:48:46 +0200

    feat: implement even number filtering

---
+++ tests/test_filtering.py
@@ -0,0 +1,9 @@
+from pytemplate.utils.filtering import filter_even_numbers
+
+
+def test_filter_even_numbers():
+    assert filter_even_numbers([1, 2, 3, 4, 5]) == [2, 4]
+    assert filter_even_numbers([10, 15, 20, 25]) == [10, 20]
+    assert filter_even_numbers([0, -2, -3, -4]) == [0, -2, -4]
+    assert filter_even_numbers([]) == []
+    assert filter_even_numbers([1, 3, 5]) == []
